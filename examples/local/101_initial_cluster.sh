174c893a2523e9973ed989884f451ecbfeca940e
ErPr <raftx24@gmail.com>
2022-01-28 21:46:05 +0100

    Update 101_initial_cluster.sh

--- examples/local/101_initial_cluster.sh
+++ examples/local/101_initial_cluster.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Copyright 2019 The Vitess Authors.
-#
+# 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-#
+# 
 #     http://www.apache.org/licenses/LICENSE-2.0
-#
+# 
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,37 +21,24 @@ source ./env.sh
 
 # start topo server
 if [ "${TOPO}" = "zk2" ]; then
-	CELL=zone1 ./scripts/zk-up.sh
+    CELL=zone1 ./scripts/zk-up.sh
 elif [ "${TOPO}" = "k8s" ]; then
-	CELL=zone1 ./scripts/k3s-up.sh
+    CELL=zone1 ./scripts/k3s-up.sh
 else
-	CELL=zone1 ./scripts/etcd-up.sh
+ CELL=zone1 ./scripts/etcd-up.sh
 fi
 
 # start vtctld
 CELL=zone1 ./scripts/vtctld-up.sh
 
 # start vttablets for keyspace commerce
-for i in 100; do
-	CELL=zone1 TABLET_UID=$i ./scripts/mysqlctl-up.sh
-	CELL=zone1 KEYSPACE=sharded TABLET_UID=$i ./scripts/vttablet-up.sh
-done
-# start vttablets for keyspace commerce
-for i in 200; do
-	CELL=zone1 TABLET_UID=$i ./scripts/mysqlctl-up.sh
-	CELL=zone1 KEYSPACE=unsharded TABLET_UID=$i ./scripts/vttablet-up.sh
+for i in 100 101 102; do
+ CELL=zone1 TABLET_UID=$i ./scripts/mysqlctl-up.sh
+ CELL=zone1 KEYSPACE=commerce TABLET_UID=$i ./scripts/vttablet-up.sh
 done
 
-# # set one of the replicas to primary
-# vtctldclient InitShardPrimary --force commerce/0 zone1-100
-
-# # create the schema
-# vtctlclient ApplySchema -sql-file create_commerce_schema.sql commerce
-
-# # create the vschema
-# vtctlclient ApplyVSchema -vschema_file vschema_commerce_initial.json commerce
+# set one of the replicas to primary
+vtctlclient -server localhost:15999 InitShardPrimary -force commerce/0 zone1-100
 
 # start vtgate
 CELL=zone1 ./scripts/vtgate-up.sh
-
-sleep 10000
